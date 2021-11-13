cff6f36a26909d02243b371a4520a1760aa60f4e
shhdq <balodis.madars@gmail.com>
2021-11-13 18:50:17 +0200

    add word in seo description

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Cybertik: - kiberdrošības pakalpojumi</title>
         <meta name="robots" content="index,follow">
-        <meta name="description" content="Kiberdrošība - mēs specializējamies socialajā inženierijā, veicam apmācības, testējam un veicam auditus. Esi pārliecināts par sava uzņēmuma drošību ar cybertik">
+        <meta name="description" content="Kiberdrošība - mēs specializējamies socialajā inženierijā, veicam apmācības, testējam un veicam drošības auditus. Esi pārliecināts par sava uzņēmuma drošību ar cybertik">
         <meta name="keywords" content="Kiberdrošība, IT drošība, Datu drošība, IT drošības apmācibas "></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
